7cea172d6d26d23282bf0bbbec28bb62b67384f1
brendan sudol <brendansudol@gmail.com>
2017-04-05 00:00:32 -0400

    add course details

--- fetch-data.py
+++ fetch-data.py
@@ -4,12 +4,14 @@ import requests
 
 from random import random
 from time import sleep
+from urllib.request import urlretrieve
 
 
 BASE_URL = 'http://www.pgatour.com/data/r'
+BASE_HOLE_URL = 'http://i.pgatour.com/image/upload/c_fill,w_720,b_rgb:424141,b_rgb:424242'
 BASE_DATA_DIR = 'data/tournaments'
 YEARS = ['2015', '2016']
-COURSE_IDS = [
+TOURNEY_IDS = [
     '002', '003', '004', '005', '006', '007', '009', '010', '011',
     '012', '013', '014', '016', '018', '019', '020', '021', '023',
     '025', '026', '027', '028', '030', '032', '033', '034', '041',
@@ -17,6 +19,12 @@ COURSE_IDS = [
     '472', '473', '474', '475', '476', '478', '480', '483', '489',
     '490', '493', '494', '500', '505', '518', '519',
 ]
+
+
+def hole_url(yr, tid, cid, num, view='full'):
+    return '{}/holes_{}_r_{}_{}_overhead_{}_{}.jpg'.format(
+        BASE_HOLE_URL, yr, tid, cid, view, num
+    )
 
 
 def save(data, path):
@@ -56,21 +64,69 @@ def fetch_and_save(url, dir_path, fname):
 
 
 def run():
-    for cid in COURSE_IDS:
-        course = fetch_and_save(
-            '{}/{}/course.json'.format(BASE_URL, cid),
-            '{}/{}'.format(BASE_DATA_DIR, cid),
-            'course.json',
-        )
-
-        if not course:
-            print('cannot find course {}'.format(cid))
-            continue
-
+    for tid in TOURNEY_IDS:
         for yr in YEARS:
+            data_url = '{}/{}/{}'.format(BASE_URL, tid, yr)
+            data_dir = '{}/{}/{}'.format(BASE_DATA_DIR, tid, yr)
+
+            # fetch course summary info
+
+            course_dir = '{}/course'.format(data_dir)
+            course_data = fetch_and_save(
+                '{}/course.json'.format(data_url),
+                course_dir,
+                'overview.json',
+            )
+
+            if not course_data:
+                print('cannot find course for {}'.format(tid))
+                continue
+
+            # fetch course details & imgs
+
+            for course in course_data['courses']:
+                print('{}...'.format(course['name']))
+                cid = course['number']
+                cid_dir = '{}/{}'.format(course_dir, cid)
+
+                maybe_make_dir(cid_dir)
+                save(course, '{}/info.json'.format(cid_dir))
+
+                holes = [
+                    {
+                        'num': num,
+                        'full': hole_url(yr, tid, cid, num, 'full'),
+                        'green': hole_url(yr, tid, cid, num, 'green'),
+                    } for num in range(1, 19)
+                ]
+                print('sample of hole URLs: {}'.format(holes[:5]))
+
+                # try first hole
+                hole1 = holes[0]['full']
+                result = requests.get(hole1, timeout=10).status_code
+                if result != 200:
+                    print('course {}: no 1st hole img ({})'.format(cid, result))
+                    continue
+
+                hole_dir = '{}/holes'.format(cid_dir)
+                maybe_make_dir(hole_dir)
+
+                for hole in holes:
+                    num = hole['num']
+                    path_start = '{}/{}'.format(hole_dir, '{0:02d}'.format(num))
+                    try:
+                        urlretrieve(hole['full'], '{}_{}.jpg'.format(path_start, 'full'))
+                        urlretrieve(hole['green'], '{}_{}.jpg'.format(path_start, 'green'))
+                    except Exception as e:
+                        print('error with {} ({})'.format(hole, str(e)))
+                    if num % 6 == 0:
+                        sleep(1 + random())
+
+            # fetch tourney (scoring) summary
+
             summary = fetch_and_save(
-                '{}/{}/{}/leaderboard-v2.json'.format(BASE_URL, cid, yr),
-                '{}/{}/{}'.format(BASE_DATA_DIR, cid, yr),
+                '{}/leaderboard-v2.json'.format(data_url),
+                data_dir,
                 'summary.json',
             )
 
@@ -78,14 +134,15 @@ def run():
                 print('no tourney summary for {}'.format(yr))
                 continue
 
-            sleep(1 + random())
+            # fetch player tourney scorecards
+
             players = [p['player_id'] for p in summary['leaderboard']['players']]
             print('{} players found'.format(len(players)))
 
             for i, pid, in enumerate(players):
                 fetch_and_save(
-                    '{}/{}/{}/scorecards/{}.json'.format(BASE_URL, cid, yr, pid),
-                    '{}/{}/{}/scorecards'.format(BASE_DATA_DIR, cid, yr),
+                    '{}/scorecards/{}.json'.format(data_url, pid),
+                    '{}/scorecards'.format(data_dir),
                     '{}.json'.format(pid),
                 )
 
